2fa9eba1c6f7f02c26b1ba113d7e7fbdde611cbd
chenr <chenr@op-mobile.com.cn>
2021-06-30 14:40:20 +0800

    eureka 服务发现  和 自我保护机制

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,7 +6,11 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author： chenr
@@ -20,23 +24,41 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Value("${server.port}")
     private String serverPort;
+
     @PostMapping("/create")
-    public CommonResult<Integer> create(@RequestBody  Payment payment){
+    public CommonResult<Integer> create(@RequestBody Payment payment) {
         int result = paymentService.save(payment);
-        log.info("插入结果:"+payment);
-        return new CommonResult<Integer>(200,"新增成功!,serverPort:"+serverPort,result);
+        log.info("插入结果:" + payment);
+        return new CommonResult<Integer>(200, "新增成功!,serverPort:" + serverPort, result);
     }
+
     @GetMapping("/getPaymentById/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable long id) {
         Payment result = paymentService.getPaymentById(id);
         if (result != null) {
-        return new CommonResult<Payment>(200,"查询成功!,serverPort:"+serverPort,result);
+            return new CommonResult<Payment>(200, "查询成功!,serverPort:" + serverPort, result);
         } else {
-            return new CommonResult<Payment>(444,"没有对应的记录!,serverPort:"+serverPort,result);
+            return new CommonResult<Payment>(444, "没有对应的记录!,serverPort:" + serverPort, result);
         }
 
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service:" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
